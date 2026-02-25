d2a5e014a37dc9d92fa19aedbeaaebb2ddff54c5
Arrush-M <arrushmurali@iisc.ac.in>
2026-02-25 12:40:52 +0530

    Added trajectory
    A bit buggy, suddenly will swap sign of x or vx.

--- aero.py
+++ aero.py
@@ -2,11 +2,12 @@ import math
 # ALL UNITS IN SI
 
 dx = 0.00001
+dt = 0.00001
 L = 0.2 # wing length
 plane = 0 # 0: suzanne, 1: super dart, 2: alkonost, 3: chinese glider
 density = 1.054 # from altitude and temp
 viscosity = 0.00001852 # from temp
-
+m = 0.0125 # mass of plane without load
 def f(x: float): # top outline of aerofoil
     return math.e * x * math.exp(-20.0*x) / 1.25
 
@@ -56,3 +57,29 @@ def vsquared_drag(airspeed, aspect, wing_angle):
     area_cs_wing = L*aspect*0.00018 # paper thickness DUMMY
     area_cs_fuselage = L*math.sin(wing_angle)*0.001 # fuselage thickness DUMMY
     return 0.5*airspeed*airspeed*density*(area_cs_fuselage*0.04 + area_cs_wing*0.09) #0.5Cd*rho*A*v^2 for each part
+
+def trajectory(init_vx, init_vy, plane, aspect, wing_angle): # without load for now
+    posn = [[0,0]]
+    vx = init_vx
+    vy = init_vy
+    theta = math.atan2(vy,vx)
+    I = (1/3)*m*L*L*aspect*aspect # placeholder
+    cp_cg = -0.05 # placeholder distance between centre of pressure, centre of gravity
+    while True:
+        x = posn[-1][0]
+        y = posn[-1][0]
+        v = math.sqrt(vx*vx + vy*vy)
+        vx -= (1/m)*(skin_friction(plane, v, aspect) + vsquared_drag(v, aspect, wing_angle))*math.cos(theta)
+        vy -= (1/m)*(skin_friction(plane, v, aspect) + vsquared_drag(v, aspect, wing_angle))*math.sin(theta)
+        vx -= (1/m)*lift(plane, v, aspect)*math.sin(theta)
+        vy += (1/m)*lift(plane, v, aspect)*math.cos(theta)
+        omega = (1/I)*lift(plane, v, aspect)*cp_cg
+        theta += omega*dt
+        x += vx*dt
+        y += vy*dt
+        posn.append([x,y])
+        if y <= 0:
+            break
+    return posn
+
+print(trajectory(0.1,0.1,0,1,0.2))
