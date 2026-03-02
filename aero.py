b81dc50f16f0565c837e6541dd54811326f35b32
Arrush-M <arrushmurali@iisc.ac.in>
2026-03-02 21:19:36 +0530

    Trajectory fixed?
    I think trajectory is fixed now. Also a bit of graphing capability added

--- aero.py
+++ aero.py
@@ -1,8 +1,10 @@
 import math
+import matplotlib.pyplot as plt
+import numpy as np
 # ALL UNITS IN SI
 
 dx = 0.00001
-dt = 0.00001
+dt = 0.001
 L = 0.2 # wing length
 plane = 0 # 0: suzanne, 1: super dart, 2: alkonost, 3: chinese glider
 density = 1.054 # from altitude and temp
@@ -23,7 +25,7 @@ def arc_length():
     return arc
 
 def lift(plane, airspeed, aspect): # Bernoulli effect lift
-    lift_P = density*airspeed*airspeed*((arc_length()/L)*(arc_length()/L) - 1)
+    lift_P = 0.5*density*airspeed*airspeed*((arc_length()/L)*(arc_length()/L) - 1)
     lift_F = 0.0
     if plane == 0:
         lift_F = lift_P*L*(L*aspect)/1.6 # L*aspect = wingspan, denominators DUMMY for now
@@ -55,25 +57,26 @@ def skin_friction(plane, airspeed, aspect):
 
 def vsquared_drag(airspeed, aspect, wing_angle):
     area_cs_wing = L*aspect*0.00018 # paper thickness DUMMY
-    area_cs_fuselage = L*math.sin(wing_angle)*0.001 # fuselage thickness DUMMY
+    area_cs_fuselage = L*math.sin(wing_angle)*0.002 # fuselage thickness DUMMY
     return 0.5*airspeed*airspeed*density*(area_cs_fuselage*0.04 + area_cs_wing*0.09) #0.5Cd*rho*A*v^2 for each part
 
-def trajectory(init_vx, init_vy, plane, aspect, wing_angle): # without load for now
+def trajectory(init_v, init_theta, plane, aspect, wing_angle): # without load for now
     posn = [[0,0]]
-    vx = init_vx
-    vy = init_vy
-    theta = math.atan2(vy,vx)
+    v = init_v
+    theta = init_theta
     I = (1/3)*m*L*L*aspect*aspect # placeholder
     cp_cg = -0.05 # placeholder distance between centre of pressure, centre of gravity
+    omega = 0.0
     while True:
         x = posn[-1][0]
-        y = posn[-1][0]
-        v = math.sqrt(vx*vx + vy*vy)
-        vx -= (1/m)*(skin_friction(plane, v, aspect) + vsquared_drag(v, aspect, wing_angle))*math.cos(theta)
-        vy -= (1/m)*(skin_friction(plane, v, aspect) + vsquared_drag(v, aspect, wing_angle))*math.sin(theta)
-        vx -= (1/m)*lift(plane, v, aspect)*math.sin(theta)
-        vy += (1/m)*lift(plane, v, aspect)*math.cos(theta)
-        omega = (1/I)*lift(plane, v, aspect)*cp_cg
+        y = posn[-1][1]
+        vx = v*math.cos(theta)
+        vy = v*math.sin(theta)
+        v -= skin_friction(plane, v, aspect)*dt/m
+        v -= vsquared_drag(v, aspect, wing_angle)*dt/m # drag force
+        v = math.sqrt(v**2 + (lift(plane, v, aspect)*dt/m)**2) # Pythagoras because lift perpendicular to velocity
+        alpha = (1/I)*lift(plane, v, aspect)*cp_cg
+        omega += alpha*dt
         theta += omega*dt
         x += vx*dt
         y += vy*dt
@@ -82,4 +85,12 @@ def trajectory(init_vx, init_vy, plane, aspect, wing_angle): # without load for 
             break
     return posn
 
-print(trajectory(0.1,0.1,0,1,0.2))
+plt.figure(dpi=100)
+posn = trajectory(5, math.pi/4, 0, 0.2, math.pi/9)
+x_vals = [p[0] for p in posn]
+y_vals = [p[1] for p in posn]
+plt.plot(x_vals, y_vals)
+plt.xlabel("Distance (m)")
+plt.ylabel("Height (m)")
+plt.title("Trajectory of a plane")
+plt.show()
